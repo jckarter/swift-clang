fbee3312c932aa5204c92a6dc547155299f44689
Jim Grosbach <grosbach@apple.com>
2011-09-12 13:42:44 -0700

    ARM64 target floating point and vector register names.

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -2386,9 +2386,25 @@ const char * const ARM64TargetInfo::GCCRegNames[] = {
   "x0",  "x1",  "x2",  "x3",  "x4",  "x5",  "x6",  "x7",
   "x8",  "x9",  "x10", "x11", "x12", "x13", "x14", "x15",
   "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23",
-  "x24", "x25", "x26", "x27", "x28", "fp",  "lr",  "sp"
+  "x24", "x25", "x26", "x27", "x28", "fp",  "lr",  "sp",
 
-  // FIXME: Float/Vector registers
+  // 32-bit floating point regsisters
+  "s0",  "s1",  "s2",  "s3",  "s4",  "s5",  "s6",  "s7",
+  "s8",  "s9",  "s10", "s11", "s12", "s13", "s14", "s15",
+  "s16", "s17", "s18", "s19", "s20", "s21", "s22", "s23",
+  "s24", "s25", "s26", "s27", "s28", "s29", "s30", "s31",
+
+  // 64-bit floating point regsisters
+  "d0",  "d1",  "d2",  "d3",  "d4",  "d5",  "d6",  "d7",
+  "d8",  "d9",  "d10", "d11", "d12", "d13", "d14", "d15",
+  "d16", "d17", "d18", "d19", "d20", "d21", "d22", "d23",
+  "d24", "d25", "d26", "d27", "d28", "d29", "d30", "d31",
+
+  // Vector registers
+  "v0",  "v1",  "v2",  "v3",  "v4",  "v5",  "v6",  "v7",
+  "v8",  "v9",  "v10", "v11", "v12", "v13", "v14", "v15",
+  "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23",
+  "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31"
 };
 
 void ARM64TargetInfo::getGCCRegNames(const char * const *&Names,
