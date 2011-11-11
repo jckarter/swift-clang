d031cf499ef59cf5a6d6714683fc067226fb651a
jahanian <fjahanian@apple.com>
2011-11-11 14:22:20 -0800

    an objc++ test for container subscripting.

---
+++ test/CodeGenObjCXX/objc-container-subscripting.mm
@@ -0,0 +1,30 @@
+// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -emit-llvm  -o /dev/null %s
+
+typedef unsigned int size_t;
+@protocol P @end
+
+@interface NSMutableArray
+- (id)objectAtIndexedSubscript:(size_t)index;
+- (void)objectAtIndexedSubscript:(size_t)index put:(id)object;
+@end
+
+struct S {
+  operator unsigned int ();
+  operator id ();
+};
+
+@interface NSMutableDictionary
+- (id)objectForKeyedSubscript:(id)key;
+- (void)setObject:(id)object forKeyedSubscript:(id)key;
+@end
+
+int main() {
+  NSMutableArray<P> * array;
+  S s;
+  id oldObject = array[s];
+
+  NSMutableDictionary<P> *dict;
+  dict[(id)s] = oldObject;
+  oldObject = dict[(id)s];
+
+}
