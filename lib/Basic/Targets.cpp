45dde9417f0be2c58bbbffccde4bb4fcc2a378b5
Jim Grosbach <grosbach@apple.com>
2012-01-31 10:47:14 -0800

    ARM64 varargs are not ABI compatible w/ nonvarargs.

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -3026,7 +3026,7 @@ public:
     Int64Type = SignedLong;
     RegParmMax = 8;
 
-    DescriptionString = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
+    DescriptionString = "e-c-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
                         "i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-"
                         "a0:0:64-n32:64";
 
