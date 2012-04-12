c0e3861e002d0db839539f63e3623ee2f84c2743
Jim Grosbach <grosbach@apple.com>
2012-04-11 18:31:18 -0700

    ARM64 tweak the predefined name for SIMD.
    s/__AARCH64_ADVSIMD__/__AARCH64_SIMD__/
    
    This is consistent with the header file name, which is general goodness.
    
    rdar://11121142

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -3065,7 +3065,7 @@ public:
     Builder.defineMacro("__arm64");
     Builder.defineMacro("__arm64__");
     Builder.defineMacro("__ARM_ARCH_8__");
-    Builder.defineMacro("__AARCH64_ADVSIMD__");
+    Builder.defineMacro("__AARCH64_SIMD__");
 
     // Target properties.
     Builder.defineMacro("_LP64");
