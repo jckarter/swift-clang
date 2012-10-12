dfd81ea3cc130218bb5e99f90b904f900491fd32
Joel Jones <joel_k_jones@apple.com>
2012-10-12 06:47:23 -0700

    Add support for one new ARM64 AdvSIMD intrinsic, vfmaq_n_f64.
    <rdar://problem/11950717>

--- test/CodeGen/arm64_vfma.c
+++ test/CodeGen/arm64_vfma.c
@@ -1,10 +1,7 @@
 // RUN: %clang -O1 -arch arm64 -S -o - -emit-llvm %s | FileCheck %s
+// Test ARM64 SIMD fused multiply add intrinsics
 
 #include <aarch64_simd.h>
-
-// old ones: vfma_f32 vfmaq_f32 vfmaq_f64 vfma_lane_f32 vfmaq_lane_f32
-//    vfmaq_lane_f64 vfma_n_f32 vfmaq_n_f32
-// new ones: vfmaq_n_f64 vfmaq_laneq_f32 vfmaq_laneq_f64 
 
 float32x2_t test_vfma_f32(float32x2_t a1, float32x2_t a2, float32x2_t a3) {
   // CHECK: test_vfma_f32
@@ -69,5 +66,14 @@ float32x4_t test_vfmaq_n_f32(float32x4_t a1, float32x4_t a2, float32_t a3) {
   // NB: the test below is deliberately lose, so that we don't depend too much
   // upon the exact IR used to select lane 0 (usually four insertelements)
   // CHECK: llvm.fma.v4f32
+  // CHECK-NEXT: ret
+}
+
+float64x2_t test_vfmaq_n_f64(float64x2_t a1, float64x2_t a2, float64_t a3) {
+  // CHECK: test_vfmaq_n_f64
+  return vfmaq_n_f64(a1, a2, a3);
+  // NB: the test below is deliberately lose, so that we don't depend too much
+  // upon the exact IR used to select lane 0 (usually two insertelements)
+  // CHECK: llvm.fma.v2f64
   // CHECK-NEXT: ret
 }
