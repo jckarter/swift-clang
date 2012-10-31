7693b0ca24fac117725c5dda15221bc65b0fe314
Daniel Dunbar <daniel@zuster.org>
2012-10-31 12:34:18 -0700

    Add Swift specific code (previously in LLVM) to getArchTypeForDarwinArchName.

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -3560,6 +3560,12 @@ llvm::Triple::ArchType darwin::getArchTypeForDarwinArchName(StringRef Str) {
     .Case("r600", llvm::Triple::r600)
     .Case("nvptx", llvm::Triple::nvptx)
     .Case("nvptx64", llvm::Triple::nvptx64)
+#ifndef __OPEN_SOURCE__
+    .Case("igil32", Triple::igil32)
+    .Case("igil64", Triple::igil64)
+#endif // !__OPEN_SOURCE__
+    .Case("gpu_32", Triple::gpu_32)
+    .Case("gpu_64", Triple::gpu_64)
     .Case("amdil", llvm::Triple::amdil)
     .Case("spir", llvm::Triple::spir)
     .Default(llvm::Triple::UnknownArch);
