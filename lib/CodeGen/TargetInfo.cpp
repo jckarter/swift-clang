f1f9fc3e5a52a22bafe06f449c7715599d1fc32d
Bob Wilson <bob.wilson@apple.com>
2012-11-02 09:05:53 -0700

    ARM64 assembly comment marker is a semicolon.  <rdar://problem/12624263>

--- lib/CodeGen/TargetInfo.cpp
+++ lib/CodeGen/TargetInfo.cpp
@@ -2884,7 +2884,7 @@ public:
     : TargetCodeGenInfo(new ARM64ABIInfo(CGT)) {}
 
   StringRef getARCRetainAutoreleasedReturnValueMarker() const {
-    return "mov\tfp, fp\t\t@ marker for objc_retainAutoreleaseReturnValue";
+    return "mov\tfp, fp\t\t; marker for objc_retainAutoreleaseReturnValue";
   }
 
   int getDwarfEHStackPointer(CodeGen::CodeGenModule &M) const {
