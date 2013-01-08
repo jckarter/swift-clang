4a5368369d8ff22bd3155205c6488e9cdad54b9c
Chad Rosier <mcrosier@apple.com>
2013-01-08 09:53:58 -0800

    Update test case due to r171440.

--- test/SemaObjC/opaque-is-access-warn.m
+++ test/SemaObjC/opaque-is-access-warn.m
@@ -18,6 +18,6 @@ static void func() {
  
   id x;
 
-  [(*x).isa self]; // expected-warning {{direct access to objective-c's isa is deprecated in favor of object_setClass() and object_getClass()}}
-  [x->isa self];   // expected-warning {{direct access to objective-c's isa is deprecated in favor of object_setClass() and object_getClass()}}
+  [(*x).isa self]; // expected-warning {{direct access to Objective-C's isa is deprecated in favor of object_setClass() and object_getClass()}}
+  [x->isa self];   // expected-warning {{direct access to Objective-C's isa is deprecated in favor of object_setClass() and object_getClass()}}
 }
