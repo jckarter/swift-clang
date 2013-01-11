35939233417aad9b5d99d4337fc8ffbddee4e8a6
Bob Wilson <bob.wilson@apple.com>
2013-01-10 22:53:07 -0800

    Fix a merge problem from 03af9402f71b0ad99f173846ea732cca7f19bd4d

--- lib/Sema/SemaExprCXX.cpp
+++ lib/Sema/SemaExprCXX.cpp
@@ -5437,7 +5437,7 @@ ExprResult Sema::IgnoredValueConversions(Expr *E) {
     // volatile lvalue with a special form, we perform an lvalue-to-rvalue
     // conversion.
     // Per <rdar://problem/11525766>, we do this in C++98 mode as well.
-    if (getLangOpts().CPlusPlus11 && E->isGLValue() &&
+    if (/*getLangOpts().CPlusPlus11 &&*/ E->isGLValue() &&
         E->getType().isVolatileQualified() &&
         IsSpecialDiscardedValue(E)) {
       ExprResult Res = DefaultLvalueConversion(E);
