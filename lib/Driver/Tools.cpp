7a4ec0c37df3e37d86fc020da48049add28428ea
Jim Grosbach <grosbach@apple.com>
2013-01-17 13:38:33 -0800

    ARM64: Add support for noredzone functions.
    When compiling kernel code (-fapple-kext or -mkernel), or when the user
    requested no red zone (-mno-red-zone), mark functions with the
    'noredzone' attribute.
    
    rdar://12565081

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -856,6 +856,13 @@ static std::string getARM64TargetCPU(const ArgList &Args) {
 
 void Clang::AddARM64TargetArgs(const ArgList &Args,
                                ArgStringList &CmdArgs) const {
+  if (!Args.hasFlag(options::OPT_mred_zone,
+                    options::OPT_mno_red_zone,
+                    true) ||
+      Args.hasArg(options::OPT_mkernel) ||
+      Args.hasArg(options::OPT_fapple_kext))
+    CmdArgs.push_back("-disable-red-zone");
+
   if (!Args.hasFlag(options::OPT_mimplicit_float,
                     options::OPT_mno_implicit_float,
                     true))
