4d6e51e541e02aa31ff78bd8fccb45f1a81c6b08
Bob Wilson <bob.wilson@apple.com>
2013-03-15 13:44:27 -0700

    Update the change for <rdar://problem/12617764> to only change the load size.
    We decided to split this change into 2 steps to ease the integration.  Since
    I had already applied the previous patch from Fariborz to the Osceola branch,
    this change updates it to match what we ended up doing for the first step on
    clang-500.2-branch.  Trunk has changed since we branched, so this isn't
    completely the same as the version on the branch.

--- lib/CodeGen/CGObjCMac.cpp
+++ lib/CodeGen/CGObjCMac.cpp
@@ -5055,16 +5055,15 @@ ObjCCommonTypesHelper::ObjCCommonTypesHelper(CodeGen::CodeGenModule &cgm)
   Int8PtrTy = CGM.Int8PtrTy;
   Int8PtrPtrTy = CGM.Int8PtrPtrTy;
   
-  // 64bit (arm64) device and 64bit simulator both use 32bit ivar offset
-  // variables. All others, preserve their existing behavior.
-  // This include, OS X x86_64  and  Windows x86_64.
-  const llvm::Triple &Triple = CGM.getTarget().getTriple();
-  if (Triple.getArch() == llvm::Triple::arm64 ||
-      (Triple.getOS() == llvm::Triple::IOS &&
-       Triple.getArch() == llvm::Triple::x86_64))
+#if 0
+  // arm64 targets use "int" ivar offset variables. All others,
+  // including OS X x86_64 and Windows x86_64, use "long" ivar offsets.
+  if (CGM.getTarget().getTriple().getArch() == llvm::Triple::arm64)
     IvarOffsetVarTy = IntTy;
   else
     IvarOffsetVarTy = LongTy;
+#endif
+  IvarOffsetVarTy = LongTy;
 
   ObjectPtrTy = Types.ConvertType(Ctx.getObjCIdType());
   PtrObjectPtrTy = llvm::PointerType::getUnqual(ObjectPtrTy);
@@ -6489,18 +6488,23 @@ llvm::Value *CGObjCNonFragileABIMac::EmitIvarOffset(
   CodeGen::CodeGenFunction &CGF,
   const ObjCInterfaceDecl *Interface,
   const ObjCIvarDecl *Ivar) {
-  llvm::Value *IvarOffsetValue =
-    CGF.Builder.CreateLoad(ObjCIvarOffsetVariable(Interface, Ivar),"ivar");
+  llvm::Value *IvarOffsetValue = ObjCIvarOffsetVariable(Interface, Ivar);
+  // For arm64, load only the low 32 bits of the ivar offset.
+  if (CGM.getTarget().getTriple().getArch() == llvm::Triple::arm64) {
+    IvarOffsetValue = CGF.Builder.CreateBitCast(IvarOffsetValue, 
+                                                CGM.Int32Ty->getPointerTo());
+  }
+  IvarOffsetValue = CGF.Builder.CreateLoad(IvarOffsetValue, "ivar");
   if (IsIvarOffsetKnownIdempotent(CGF, Interface, Ivar))
     cast<llvm::LoadInst>(IvarOffsetValue)
       ->setMetadata(CGM.getModule().getMDKindID("invariant.load"),
                     llvm::MDNode::get(VMContext, ArrayRef<llvm::Value*>()));
 
-
   // This could be 32bit int or 64bit integer depending on the architecture.
   // Cast it to 64bit integer value, if it is a 32bit integer ivar offset value
   //  as this is what caller always expectes.
-  if (ObjCTypes.IvarOffsetVarTy == ObjCTypes.IntTy)
+  if (CGM.getTarget().getTriple().getArch() == llvm::Triple::arm64)
+  // if (ObjCTypes.IvarOffsetVarTy == ObjCTypes.IntTy)
     IvarOffsetValue =
       CGF.Builder.CreateIntCast(IvarOffsetValue, ObjCTypes.LongTy, 
                                 true, "ivar.conv");
