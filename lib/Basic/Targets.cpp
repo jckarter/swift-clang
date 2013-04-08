0fcfff1c8198ec359cd94e1018dca8a048f492c2
Bob Wilson <bob.wilson@apple.com>
2013-04-08 13:52:05 -0700

    Stop predefining __ARM_ARCH_8__ for 64-bit code. <rdar://problem/12148175>

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -4050,7 +4050,6 @@ public:
     Builder.defineMacro("__arm64");
     Builder.defineMacro("__arm64__");
     Builder.defineMacro("__aarch64__");
-    Builder.defineMacro("__ARM_ARCH_8__");
     Builder.defineMacro("__ARM64_ARCH_8__");
     Builder.defineMacro("__AARCH64_SIMD__");
     Builder.defineMacro("__ARM_NEON__");
