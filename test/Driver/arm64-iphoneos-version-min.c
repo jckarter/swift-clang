1327b1cfcb138597197fb9ecfc121ed95a39ba58
Bob Wilson <bob.wilson@apple.com>
2013-04-23 12:47:33 -0700

    Revert "[driver] Emit an error if -miphoneos-version-min is less than 7.0.0."
    This reverts commit c57d66236455800f72370e18e44abf4408243e3f.
    
    We've decided to take a different approach.

--- test/Driver/arm64-iphoneos-version-min.c
+++
@@ -1,8 +0,0 @@
-// RUN: %clang -arch arm64 -miphoneos-version-min=6.0.0bar %s -verify 2>&1 | FileCheck -check-prefix=ARM64-INV %s
-// RUN: %clang -arch arm64 -miphoneos-version-min=6.0.0 %s -verify 2>&1 | FileCheck -check-prefix=ARM64-BAD %s
-// RUN: %clang -arch arm64 -miphoneos-version-min=7.0.0 %s -verify 2>&1| FileCheck -check-prefix=ARM64-GOOD %s
-
-// ARM64-INV: error: invalid version number in '-miphoneos-version-min=6.0.0bar'
-// ARM64-INV-NOT: invalid deployment target
-// ARM64-BAD: invalid deployment target '-miphoneos-version-min=6.0.0' for architecture 'arm64' (requires '7.0.0' or later)
-// ARM64-GOOD-NOT: invalid deployment target '-miphoneos-version-min=7.0.0' for architecture 'arm64' (requires '7.0.0' or later)
