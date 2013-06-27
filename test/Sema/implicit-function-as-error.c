22dc39f4c8d9a288842c7d744c35c3587e75cbde
Tim Northover <tnorthover@apple.com>
2013-06-27 13:38:50 +0100

    ARM64: enforce Darwin host platform if Darwin-specific flags used.

--- test/Sema/implicit-function-as-error.c
+++ test/Sema/implicit-function-as-error.c
@@ -1,5 +1,5 @@
-// RUN: %clang -arch x86_64 -mios-simulator-version-min=7 -fsyntax-only %s -Xclang -verify
-// RUN: %clang -arch arm64 -mios-version-min=7 -fsyntax-only %s -Xclang -verify
+// RUN: %clang -target x86_64-apple-darwin -mios-simulator-version-min=7 -fsyntax-only %s -Xclang -verify
+// RUN: %clang -target x86_64-apple-darwin -arch arm64 -target x86_64-apple-darwin -mios-version-min=7 -fsyntax-only %s -Xclang -verify
 
 // For 64-bit iOS, automatically promote -Wimplicit-function-declaration
 // to an error.
--- test/SemaObjC/opaque-is-access-warn.m
+++ test/SemaObjC/opaque-is-access-warn.m
@@ -1,6 +1,6 @@
-// RUN: %clang -arch arm64 -mios-version-min=7 -fsyntax-only -Wdeprecated-objc-isa-usage %s -Xclang -verify
-// RUN: %clang -arch arm64 -mios-version-min=7 -fsyntax-only %s -Xclang -verify
-// RUN: %clang -arch x86_64 -mios-simulator-version-min=7 -fsyntax-only -Wdeprecated-objc-isa-usage %s -Xclang -verify
+// RUN: %clang -target x86_64-apple-darwin -arch arm64 -mios-version-min=7 -fsyntax-only -Wdeprecated-objc-isa-usage %s -Xclang -verify
+// RUN: %clang -target x86_64-apple-darwin -arch arm64 -mios-version-min=7 -fsyntax-only %s -Xclang -verify
+// RUN: %clang -target x86_64-apple-darwin -mios-simulator-version-min=7 -fsyntax-only -Wdeprecated-objc-isa-usage %s -Xclang -verify
 // rdar://10709102
 
 typedef struct objc_object {
