41bc3b4aa5bcff3a4b770199f9f5c0296b1ad605
Tim Northover <tnorthover@apple.com>
2013-06-27 13:37:39 +0100

    ARM64: make test agnostic about extern symbol names
    This allows it to pass when arm64-linux is the default triple and names don't
    get '_' prepended.

--- test/CodeGen/arm64_vecCmpBr.c
+++ test/CodeGen/arm64_vecCmpBr.c
@@ -6,12 +6,12 @@ unsigned bar();
 
 // Branch if any lane of V0 is zero; 64 bit => !min
 unsigned anyZero64(uint16x4_t a) {
-// CHECK: _anyZero64:
+// CHECK: anyZero64:
 // CHECK: uminv.8b b[[REGNO1:[0-9]+]], v0
 // CHECK-NEXT: fmov w[[REGNO2:[0-9]+]], s[[REGNO1]]
-// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[A-Z_0-9]+]]
+// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[.A-Z_0-9]+]]
 // CHECK: [[LABEL]]:
-// CHECK-NEXT: b _bar
+// CHECK-NEXT: b {{_bar|bar}}
   if (!vminv_u8(a))
     return bar();
   return 0;
@@ -19,12 +19,12 @@ unsigned anyZero64(uint16x4_t a) {
 
 // Branch if any lane of V0 is zero; 128 bit => !min
 unsigned anyZero128(uint16x8_t a) {
-// CHECK: _anyZero128:
+// CHECK: anyZero128:
 // CHECK: uminv.16b b[[REGNO1:[0-9]+]], v0
 // CHECK-NEXT: fmov w[[REGNO2:[0-9]+]], s[[REGNO1]]
-// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[A-Z_0-9]+]]
+// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[.A-Z_0-9]+]]
 // CHECK: [[LABEL]]:
-// CHECK-NEXT: b _bar
+// CHECK-NEXT: b {{_bar|bar}}
   if (!vminvq_u8(a))
     return bar();
   return 0;
@@ -32,10 +32,10 @@ unsigned anyZero128(uint16x8_t a) {
 
 // Branch if any lane of V0 is non-zero; 64 bit => max
 unsigned anyNonZero64(uint16x4_t a) {
-// CHECK: _anyNonZero64:
+// CHECK: anyNonZero64:
 // CHECK: umaxv.8b b[[REGNO1:[0-9]+]], v0
 // CHECK-NEXT: fmov w[[REGNO2:[0-9]+]], s[[REGNO1]]
-// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[A-Z_0-9]+]]
+// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[.A-Z_0-9]+]]
 // CHECK: [[LABEL]]:
 // CHECK-NEXT: movz w0, #0
   if (vmaxv_u8(a))
@@ -45,10 +45,10 @@ unsigned anyNonZero64(uint16x4_t a) {
 
 // Branch if any lane of V0 is non-zero; 128 bit => max
 unsigned anyNonZero128(uint16x8_t a) {
-// CHECK: _anyNonZero128:
+// CHECK: anyNonZero128:
 // CHECK: umaxv.16b b[[REGNO1:[0-9]+]], v0
 // CHECK-NEXT: fmov w[[REGNO2:[0-9]+]], s[[REGNO1]]
-// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[A-Z_0-9]+]]
+// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[.A-Z_0-9]+]]
 // CHECK: [[LABEL]]:
 // CHECK-NEXT: movz w0, #0
   if (vmaxvq_u8(a))
@@ -58,12 +58,12 @@ unsigned anyNonZero128(uint16x8_t a) {
 
 // Branch if all lanes of V0 are zero; 64 bit => !max
 unsigned allZero64(uint16x4_t a) {
-// CHECK: _allZero64:
+// CHECK: allZero64:
 // CHECK: umaxv.8b b[[REGNO1:[0-9]+]], v0
 // CHECK-NEXT: fmov w[[REGNO2:[0-9]+]], s[[REGNO1]]
-// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[A-Z_0-9]+]]
+// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[.A-Z_0-9]+]]
 // CHECK: [[LABEL]]:
-// CHECK-NEXT: b _bar
+// CHECK-NEXT: b {{_bar|bar}}
   if (!vmaxv_u8(a))
     return bar();
   return 0;
@@ -71,12 +71,12 @@ unsigned allZero64(uint16x4_t a) {
 
 // Branch if all lanes of V0 are zero; 128 bit => !max
 unsigned allZero128(uint16x8_t a) {
-// CHECK: _allZero128:
+// CHECK: allZero128:
 // CHECK: umaxv.16b b[[REGNO1:[0-9]+]], v0
 // CHECK-NEXT: fmov w[[REGNO2:[0-9]+]], s[[REGNO1]]
-// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[A-Z_0-9]+]]
+// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[.A-Z_0-9]+]]
 // CHECK: [[LABEL]]:
-// CHECK-NEXT: b _bar
+// CHECK-NEXT: b {{_bar|bar}}
   if (!vmaxvq_u8(a))
     return bar();
   return 0;
@@ -84,10 +84,10 @@ unsigned allZero128(uint16x8_t a) {
 
 // Branch if all lanes of V0 are non-zero; 64 bit => min
 unsigned allNonZero64(uint16x4_t a) {
-// CHECK: _allNonZero64:
+// CHECK: allNonZero64:
 // CHECK: uminv.8b b[[REGNO1:[0-9]+]], v0
 // CHECK-NEXT: fmov w[[REGNO2:[0-9]+]], s[[REGNO1]]
-// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[A-Z_0-9]+]]
+// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[.A-Z_0-9]+]]
 // CHECK: [[LABEL]]:
 // CHECK-NEXT: movz w0, #0
   if (vminv_u8(a))
@@ -97,10 +97,10 @@ unsigned allNonZero64(uint16x4_t a) {
 
 // Branch if all lanes of V0 are non-zero; 128 bit => min
 unsigned allNonZero128(uint16x8_t a) {
-// CHECK: _allNonZero128:
+// CHECK: allNonZero128:
 // CHECK: uminv.16b b[[REGNO1:[0-9]+]], v0
 // CHECK-NEXT: fmov w[[REGNO2:[0-9]+]], s[[REGNO1]]
-// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[A-Z_0-9]+]]
+// CHECK-NEXT: cbz w[[REGNO2]], [[LABEL:[.A-Z_0-9]+]]
 // CHECK: [[LABEL]]:
 // CHECK-NEXT: movz w0, #0
   if (vminvq_u8(a))
