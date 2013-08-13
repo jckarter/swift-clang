63df49a7ddb738909702d465731953a549d5c4a4
Bob Wilson <bob.wilson@apple.com>
2013-08-12 20:46:32 -0700

    Minor tweak of a FileCheck pattern to be less specific.

--- test/Driver/arm64-iphoneos-version-min.c
+++ test/Driver/arm64-iphoneos-version-min.c
@@ -5,4 +5,4 @@
 // ARM64-INV: error: invalid version number in '-miphoneos-version-min=6.0.0bar'
 // ARM64-INV-NOT: invalid deployment target
 // ARM64-BAD: invalid deployment target '-miphoneos-version-min=5.0.0' for architecture 'arm64' (requires '6.0.0' or later)
-// ARM64-GOOD-NOT: invalid deployment target '-miphoneos-version-min=6.0.0' for architecture 'arm64' (requires '6.0.0' or later)
+// ARM64-GOOD-NOT: invalid deployment target
