b950d5a5226167cf74da495345f4d0518a80ddc9
Bob Wilson <bob.wilson@apple.com>
2013-08-28 16:37:17 -0700

    Add a test for arm64 defaulting to iOS 7. <rdar://problem/14852568>

--- test/Driver/target-triple-deployment.c
+++ test/Driver/target-triple-deployment.c
@@ -7,6 +7,7 @@
 // RUN: %clang -target armv7-apple-ios0.0 -### %t.o 2>> %t.log
 // RUN: %clang -target armv7-apple-ios1.2.3 -### %t.o 2>> %t.log
 // RUN: %clang -target armv7-apple-ios5.0 -### %t.o 2>> %t.log
+// RUN: %clang -target arm64-apple-ios -### %t.o 2>> %t.log
 //
 // RUN: FileCheck %s < %t.log
 
@@ -31,3 +32,6 @@
 // CHECK: {{ld(.exe)?"}}
 // CHECK: -iphoneos_version_min
 // CHECK: 5.0.0
+// CHECK: {{ld(.exe)?"}}
+// CHECK: -iphoneos_version_min
+// CHECK: 7.0.0
