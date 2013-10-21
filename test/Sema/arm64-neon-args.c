0ab9560d58383368f3e76d9362d76313ca039a26
Tim Northover <tnorthover@apple.com>
2013-10-21 16:34:15 +0100

    ARM64: fix type specification of vmlsq intrinsics
    This also makes a bunch of benign changes to other strings. These are
    technically more correct but the code in NeonEmitter.cpp completely ignores
    them and has hard-coded knowledge of the required type.
    
    The only actual change to aarch64_simd.h is for the vmlsq_lane_* intrinsics.
    
    rdar://problem/15256199

--- test/Sema/arm64-neon-args.c
+++ test/Sema/arm64-neon-args.c
@@ -6,3 +6,8 @@
 void vcopy_reject(float32x4_t vOut0, float32x4_t vAlpha, int t) {
   vcopyq_lane_f32(vOut0, 1, vAlpha, t); // expected-error {{argument to '__builtin_arm64_vcopyq_lane_v' must be a constant integer}}
 }
+
+// rdar://problem/15256199
+float32x4_t test_vmlsq_lane(float32x4_t accum, float32x4_t lhs, float32x2_t rhs) {
+  return vmlsq_lane_f32(accum, lhs, rhs, 1);
+}
