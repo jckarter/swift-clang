818afe83eaf3a3ab372a0aec94bdb808fa082abd
Manman Ren <mren@apple.com>
2013-12-09 17:03:58 -0800

    Update testing case due to r196784.

--- test/CodeGenCXX/arm64-constructor-return.cpp
+++ test/CodeGenCXX/arm64-constructor-return.cpp
@@ -10,10 +10,10 @@ S::S() {
   iField = 1;
 };
 
+// CHECK: %struct.S* @_ZN1SC2Ev(%struct.S* returned %this)
+
 // CHECK: %struct.S* @_ZN1SC1Ev(%struct.S* returned %this)
 // CHECK: [[THISADDR:%[a-zA-z0-9.]+]] = alloca %struct.S*
 // CHECK: store %struct.S* %this, %struct.S** [[THISADDR]]
 // CHECK: [[THIS1:%.*]] = load %struct.S** [[THISADDR]]
 // CHECK: ret %struct.S* [[THIS1]]
-
-// CHECK: %struct.S* @_ZN1SC2Ev(%struct.S* returned %this)
