9743f6a1dc6b9196c01c3a748570cea08a90a2b5
Tim Northover <tnorthover@apple.com>
2014-01-06 19:34:42 +0000

    ARM64: specify sensible DataLayout after Rafael's refactoring.

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -4304,9 +4304,10 @@ public:
     LongDoubleWidth = LongDoubleAlign = 128;
     LongDoubleFormat = &llvm::APFloat::IEEEquad;
 
-    DescriptionString = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
-                        "i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-"
-                        "a0:0:64-n32:64-S128";
+    if (Triple.isOSBinFormatMachO())
+      DescriptionString = "e-m:o-i64:64-i128:128-n32:64-S128";
+    else
+      DescriptionString = "e-m:e-i64:64-i128:128-n32:64-S128";
 
     // {} in inline assembly are neon specifiers, not assembly variant
     // specifiers.
