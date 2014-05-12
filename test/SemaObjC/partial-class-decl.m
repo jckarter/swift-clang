d244a7771498d6bf71d89cf9497a82b5a4e3025d
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2014-05-12 13:43:09 -0700

    [test] Rename Shiny -> Swift in 'test/SemaObjC/partial-class-decl.m'

--- test/SemaObjC/partial-class-decl.m
+++ test/SemaObjC/partial-class-decl.m
@@ -1,64 +1,64 @@
 // RUN: %clang_cc1  -triple x86_64-apple-darwin10 -fsyntax-only -verify %s
 // rdar://16076729
 
-@partial_interface MyShinyClass;  // expected-note {{'MyShinyClass' declared here}}
+@partial_interface MySwiftClass;  // expected-note {{'MySwiftClass' declared here}}
 
-@partial_interface MyShinyClassDup; // expected-note {{previous declaration is here}}
+@partial_interface MySwiftClassDup; // expected-note {{previous declaration is here}}
 
-@partial_interface MyShinyClassDup; // expected-error {{duplicate partial class declaration for class 'MyShinyClassDup'}}
+@partial_interface MySwiftClassDup; // expected-error {{duplicate partial class declaration for class 'MySwiftClassDup'}}
 
 
 @interface DeclaredAsObjCClass @end // expected-note {{previous definition is here}}
 
 @partial_interface DeclaredAsObjCClass; // expected-error {{duplicate partial class declaration for class 'DeclaredAsObjCClass'}}
 
-typedef int ShinyClassDeclaredAsTypedefInt; // expected-note {{previous definition is here}}
+typedef int SwiftClassDeclaredAsTypedefInt; // expected-note {{previous definition is here}}
 
-@partial_interface ShinyClassDeclaredAsTypedefInt; // expected-error {{redefinition of 'ShinyClassDeclaredAsTypedefInt' as different kind of symbol}}
+@partial_interface SwiftClassDeclaredAsTypedefInt; // expected-error {{redefinition of 'SwiftClassDeclaredAsTypedefInt' as different kind of symbol}}
 
 
-@partial_interface MyShinyClassMustMatchWithShinyClass; // expected-note {{previous definition is here}}
+@partial_interface MySwiftClassMustMatchWithSwiftClass; // expected-note {{previous definition is here}}
 
-@interface MyShinyClassMustMatchWithShinyClass @end; // expected-error {{attempting to define an ordinary class for partial class 'MyShinyClassMustMatchWithShinyClass'}}
+@interface MySwiftClassMustMatchWithSwiftClass @end; // expected-error {{attempting to define an ordinary class for partial class 'MySwiftClassMustMatchWithSwiftClass'}}
 
-@class ForwardShinyClass;
-@partial_interface ForwardShinyClass; // expected-note {{previous declaration is here}}
-@class ForwardShinyClass;
-@partial_interface ForwardShinyClass; // expected-error {{duplicate partial class declaration for class 'ForwardShinyClass'}}
+@class ForwardSwiftClass;
+@partial_interface ForwardSwiftClass; // expected-note {{previous declaration is here}}
+@class ForwardSwiftClass;
+@partial_interface ForwardSwiftClass; // expected-error {{duplicate partial class declaration for class 'ForwardSwiftClass'}}
 
-@partial_interface ShinyClassWithNoDefSuperShinyClass : MyShinySuperClass; // expected-error {{cannot find interface declaration for 'MyShinySuperClass', superclass of 'ShinyClassWithNoDefSuperShinyClass'}}
+@partial_interface SwiftClassWithNoDefSuperSwiftClass : MySwiftSuperClass; // expected-error {{cannot find interface declaration for 'MySwiftSuperClass', superclass of 'SwiftClassWithNoDefSuperSwiftClass'}}
 
 typedef int BogusSuperClass; // expected-note {{previous definition is here}}
-@partial_interface ShinyClassWithSymbol : BogusSuperClass; // expected-error {{redefinition of 'BogusSuperClass' as different kind of symbol}}
+@partial_interface SwiftClassWithSymbol : BogusSuperClass; // expected-error {{redefinition of 'BogusSuperClass' as different kind of symbol}}
 
-@partial_interface ShinyRootClass;
-@partial_interface ValidShinyClassWithRoot : ShinyRootClass; // Ok. Root is another partial class.
+@partial_interface SwiftRootClass;
+@partial_interface ValidSwiftClassWithRoot : SwiftRootClass; // Ok. Root is another partial class.
 
-@partial_interface ValidShinyClassWithObjCRoot : DeclaredAsObjCClass; // Ok. Root is ObjC class
+@partial_interface ValidSwiftClassWithObjCRoot : DeclaredAsObjCClass; // Ok. Root is ObjC class
 
 @class MyAttemptToUseForwardClass; // expected-note {{forward declaration of class here}}
 
-@partial_interface ShinyClassUsingForwardSuper : MyAttemptToUseForwardClass; // expected-error {{attempting to use the forward class 'MyAttemptToUseForwardClass' as superclass of 'ShinyClassUsingForwardSuper'}}
+@partial_interface SwiftClassUsingForwardSuper : MyAttemptToUseForwardClass; // expected-error {{attempting to use the forward class 'MyAttemptToUseForwardClass' as superclass of 'SwiftClassUsingForwardSuper'}}
 
 @interface ObjCRootClass @end
 
-@partial_interface MyShinyRootClass : ObjCRootClass; // expected-note {{previous definition is here}}
+@partial_interface MySwiftRootClass : ObjCRootClass; // expected-note {{previous definition is here}}
 
-@interface ObjCClass : MyShinyRootClass
+@interface ObjCClass : MySwiftRootClass
 @end
 
-@partial_interface MyShiny2ndClass : ObjCClass;
+@partial_interface MySwift2ndClass : ObjCClass;
 
-@partial_interface MyShinySubClass : MyShiny2ndClass;
+@partial_interface MySwiftSubClass : MySwift2ndClass;
 
-@interface ObjCSubClass : MyShinySubClass
+@interface ObjCSubClass : MySwiftSubClass
 @end
 
-@interface MyShinyRootClass // expected-error {{attempting to define an ordinary class for partial class 'MyShinyRootClass'}}
+@interface MySwiftRootClass // expected-error {{attempting to define an ordinary class for partial class 'MySwiftRootClass'}}
 @end
 
 __attribute((objc_complete_definition))
-@interface MyShinySubClass : MyShiny2ndClass
+@interface MySwiftSubClass : MySwift2ndClass
 @end
 
 @partial_interface foo; // expected-note {{class is declared here}}
