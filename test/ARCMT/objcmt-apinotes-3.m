e9764bf530bfdd502fbcbb1b5da546d6c1f86692
Fariborz Jahanian <fjahanian@apple.com>
2014-10-31 10:34:58 -0700

    Patch to modernize SDKs to move API notes to the SDK headers. rdar://18523118

---
+++ test/ARCMT/objcmt-apinotes-3.m
@@ -0,0 +1,8 @@
+// RUN: rm -rf %t/APINotesCache
+// RUN: rm -rf %t
+// RUN: %clang_cc1 -mt-migrate-directory %t -objcmt-migrate-apinotes -fapinotes -fapinotes-cache-path=%t/APINotesCache -I %S/../APINotes/Inputs/Headers -F %S/../APINotes/Inputs/Frameworks %s
+// RUN: c-arcmt-test -mt-migrate-directory %t | arcmt-test -verify-transformed-files %S/SomeKit_Private.h.result %S/SomeKit.h.result
+// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -x objective-c %S/SomeKit.h.result
+// rdar://18523118
+
+#import <SomeKit/SomeKit_Private.h>
