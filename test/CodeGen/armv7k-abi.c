3e8ea5c80c9accd14b1617b5dd086641cf809941
Tim Northover <tnorthover@apple.com>
2014-10-31 11:58:35 -0700

    ARMv7k: test handling of non-HFA structs in the new ABI.
    We're adopting ARM64 conventions of deciding based on whether the struct is
    smaller or bigger than 16 bytes. We should also test the precise coerced type
    since ARM64-32 code is expected to depend on this.
    
    rdar://problem/18562057

--- test/CodeGen/armv7k-abi.c
+++ test/CodeGen/armv7k-abi.c
@@ -17,3 +17,37 @@ typedef struct {
 // CHECK: void @no_padding(i32 %r0, i32 %r1, i32 %r2, [4 x double] %d0_d3.coerce, [4 x double] %d4_d7.coerce, [4 x double] %sp.coerce, i64 %split)
 void no_padding(int r0, int r1, int r2, BigHFA d0_d3, BigHFA d4_d7, BigHFA sp,
                 long long split) {}
+
+// Structs larger than 16 bytes should be passed indirectly in space allocated
+// by the caller (a pointer to this storage should be what occurs in the arg
+// list).
+
+typedef struct {
+  float x;
+  long long y;
+  double z;
+} BigStruct;
+
+// CHECK: define void @big_struct_indirect(%struct.BigStruct* align 8 %b)
+void big_struct_indirect(BigStruct b) {}
+
+// Structs smaller than 16 bytes should be passed directly, and coerced to
+// either [N x i32] or [N x i64] depending on alignment requirements.
+
+typedef struct {
+  float x;
+  int y;
+  double z;
+} SmallStruct;
+
+// CHECK: define void @small_struct_direct([2 x i64] %s.coerce)
+void small_struct_direct(SmallStruct s) {}
+
+typedef struct {
+  float x;
+  int y;
+  int z;
+} SmallStructSmallAlign;
+
+// CHECK: define void @small_struct_align_direct([3 x i32] %s.coerce)
+void small_struct_align_direct(SmallStructSmallAlign s) {}
