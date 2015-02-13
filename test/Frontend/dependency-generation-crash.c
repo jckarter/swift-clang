12aa4e21effa47d30872ee3690fc9bb4f2acdec5
Steven Wu <stevenwu@apple.com>
2015-02-13 10:35:28 -0800

    Disable dependency-generate-crash.c test
    Our buildbots run test with sudo.
    chmod 0 will not stop sudo reading and writing the file.
    Disable the test temporarily.

--- test/Frontend/dependency-generation-crash.c
+++ test/Frontend/dependency-generation-crash.c
@@ -1,6 +1,6 @@
 // RUN: touch %t
 // RUN: chmod 0 %t
-// RUN: not %clang_cc1 -E -dependency-file bla -MT %t -MP -o %t -x c /dev/null 2>&1 | FileCheck %s
+// not %clang_cc1 -E -dependency-file bla -MT %t -MP -o %t -x c /dev/null 2>&1 | FileCheck %s
 // RUN: rm -f %t
 
 // CHECK: error: unable to open output file
