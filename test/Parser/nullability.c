cab5192744cdb404a2b2b9f219f989d0bed25ae5
Jordan Rose <jordan_rose@apple.com>
2015-03-05 14:59:30 -0800

    Put "'__nonnull' is an extension" under a separate -Wnullability-extension.
    ...rather than the general -Wnullability. It's still off-by-default, but
    those who turn on -pedantic probably want to silence just this extension
    warning. Conversely, someone who actually /does/ turn nullability warnings
    off might still want to know if they use a nullability specifier in supposedly
    portable code.

--- test/Parser/nullability.c
+++ test/Parser/nullability.c
@@ -2,6 +2,11 @@
 
 __nonnull int *ptr; // expected-warning{{type nullability specifier '__nonnull' is a Clang extension}}
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnullability-extension"
+__nonnull int *ptr2; // no-warning
+#pragma clang diagnostic pop
+
 #if __has_feature(nullability)
 #  error Nullability should not be supported in C under -pedantic -std=c99
 #endif
