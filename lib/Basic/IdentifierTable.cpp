a7fc327b0781172a1f585dce1bd8fd14dd0d1ec1
David Majnemer <david.majnemer@gmail.com>
2015-03-18 04:15:23 +0000

    MSVC Compat: Permit char16_t, char32_t and _Atomic when targeting > 2013
    We disabled support for _Atomic because the STL had name conflicts,
    they've been resolved in 2015.  Similarly, reenable char16_t and
    char32_t.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@232611 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Basic/IdentifierTable.cpp
+++ lib/Basic/IdentifierTable.cpp
@@ -106,11 +106,11 @@ namespace {
     KEYOPENCL = 0x200,
     KEYC11 = 0x400,
     KEYARC = 0x800,
-    KEYNOMS = 0x01000,
+    KEYNOMS18 = 0x01000,
     WCHARSUPPORT = 0x02000,
     HALFSUPPORT = 0x04000,
     KEYOBJC2 = 0x08000,
-    KEYALL = (0xffff & ~KEYNOMS) // Because KEYNOMS is used to exclude.
+    KEYALL = (0xffff & ~KEYNOMS18) // Because KEYNOMS is used to exclude.
   };
 
   /// \brief How a keyword is treated in the selected standard.
@@ -157,7 +157,8 @@ static void AddKeyword(StringRef Keyword,
   KeywordStatus AddResult = getKeywordStatus(LangOpts, Flags);
 
   // Don't add this keyword under MSVCCompat.
-  if (LangOpts.MSVCCompat && (Flags & KEYNOMS))
+  if (LangOpts.MSVCCompat && (Flags & KEYNOMS18) &&
+      !LangOpts.isCompatibleWithMSVC(19))
      return;
   // Don't add this keyword if disabled in this language.
   if (AddResult == KS_Disabled) return;
