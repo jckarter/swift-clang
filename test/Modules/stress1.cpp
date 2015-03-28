422f8db0e1a24739956a4c57cadd088e433d4cae
Adrian Prantl <aprantl@apple.com>
2015-03-27 21:00:50 -0700

    Revert "Temporarily disable these tests to keep the bots going"
    This reverts commit 16659e90151bb9de393c85965c8bf4e7526f138e.

--- test/Modules/stress1.cpp
+++ test/Modules/stress1.cpp
@@ -15,7 +15,7 @@
 // RUN:   -emit-module -fmodule-name=m00 -o %t/m00_check.pcm \
 // RUN:   Inputs/stress1/module.modulemap
 //
-// FIXME: diff %t/m00.pcm %t/m00_check.pcm
+// RUN: diff %t/m00.pcm %t/m00_check.pcm
 //
 // RUN: %clang_cc1 -fmodules -x c++ -std=c++11 -fdelayed-template-parsing \
 // RUN:   -I Inputs/stress1 \
@@ -31,7 +31,7 @@
 // RUN:   -emit-module -fmodule-name=m01 -o %t/m01_check.pcm \
 // RUN:   Inputs/stress1/module.modulemap
 //
-// FIXME: diff %t/m01.pcm %t/m01_check.pcm
+// RUN: diff %t/m01.pcm %t/m01_check.pcm
 //
 // RUN: %clang_cc1 -fmodules -x c++ -std=c++11 \
 // RUN:   -I Inputs/stress1 \
@@ -69,7 +69,7 @@
 // RUN:   -emit-module -fmodule-name=merge00 -o %t/merge00_check.pcm \
 // RUN:   Inputs/stress1/module.modulemap
 //
-// FIXME: diff %t/merge00.pcm %t/merge00_check.pcm
+// RUN: diff %t/merge00.pcm %t/merge00_check.pcm
 //
 // RUN: %clang_cc1 -fmodules -x c++ -std=c++11 \
 // RUN:   -I Inputs/stress1 \
