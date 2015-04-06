e74c0c0b81359d8879619e7835a9a61d19cd5349
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2015-04-06 22:04:18 +0000

    DebugInfo: Use DILexicalBlockFile::getContext() over getScope()
    `getScope()` passes the scope back through a `DILexicalBlock` even
    though the underlying pointer may be an incompatible `MDSubprogram`.
    Just use `getContext()` directly.
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@234245 91177308-0d34-0410-b5e6-96231b3b80d8
    (cherry picked from commit adca9604f182d96f3acf6fc58dfc77d23ebd6625)

--- lib/CodeGen/CGDebugInfo.cpp
+++ lib/CodeGen/CGDebugInfo.cpp
@@ -132,7 +132,7 @@ void CGDebugInfo::setLocation(SourceLocation Loc) {
   if (Scope.isLexicalBlockFile()) {
     llvm::DILexicalBlockFile LBF = llvm::DILexicalBlockFile(Scope);
     llvm::DIDescriptor D = DBuilder.createLexicalBlockFile(
-        LBF.getScope(), getOrCreateFile(CurLoc));
+        LBF.getContext(), getOrCreateFile(CurLoc));
     llvm::MDNode *N = D;
     LexicalBlockStack.pop_back();
     LexicalBlockStack.emplace_back(N);
