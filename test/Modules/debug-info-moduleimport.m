71179a3bfa40ed9647cf5e2b53c17084e270f56b
Adrian Prantl <aprantl@apple.com>
2015-05-14 15:40:13 -0700

    Upgrade a stale testcase.

--- test/Modules/debug-info-moduleimport.m
+++ test/Modules/debug-info-moduleimport.m
@@ -1,8 +1,8 @@
 // RUN: rm -rf %t
-// RUN: %clang_cc1 -fmodules -x objective-c -fmodules-cache-path=%t -DWANT_FOO=1 -emit-module -fmodule-name=config %S/Inputs/module.map
-
-// CHECK: ![[CU:.*]] = {{.*}}; [ DW_TAG_compile_unit ]
-@import Module;
-// CHECK: !{!"0x8\00[[@LINE-1]]\00", ![[CU]], ![[MODULE:.*]]} ; [ DW_TAG_imported_declaration ]
-// CHECK: ![[MODULE]] = !{!"0x1e\00Module\00{{.*}}Inputs/Module.framework/Headers\00", ![[PCM:.*]], null} ; [ DW_TAG_module ] [Module]
+// RUN: %clang_cc1 -g -dwarf-ext-refs -fmodules -DMODULES -fmodules-cache-path=%t %s -I %S/Inputs -I %t -emit-llvm -o - | FileCheck %s
+  
+// CHECK: ![[CU:.*]] = distinct !DICompileUnit
+@import DebugModule;
+// CHECK: !DIImportedEntity(tag: DW_TAG_imported_declaration, scope: ![[CU]], entity: ![[MODULE:.*]], line: 5)
+// CHECK: ![[MODULE]] = !MDModule(name: "DebugModule", scope: null, file: ![[PCM:.*]])
 // CHECK: ![[PCM]] = {{.*}}.pcm
