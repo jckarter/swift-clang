754debf85ef232db3901c5fbe52c2ea190d1eea1
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2015-06-19 16:02:48 -0700

    Bitcode: Skip -emit-llvm-uselists for -fembed-bitcode
    As with LTO, serializing to/from bitcode is a standard part of the
    `-fembed-bitcode` flow.  It's not necessary to regenerate the same
    use-list order.
    
    rdar://20651301

--- test/Driver/embed-bitcode.c
+++ test/Driver/embed-bitcode.c
@@ -5,6 +5,7 @@
 // RUN: %clang %s -fembed-bitcode 2>&1 -### | FileCheck %s -check-prefix=CHECK-CC
 // CHECK-CC: -cc1
 // CHECK-CC: -emit-llvm-bc
+// CHECK-CC-NOT: -emit-llvm-uselists
 // CHECK-CC: -cc1
 // CHECK-CC: -emit-obj
 // CHECK-CC: -fembed-bitcode
