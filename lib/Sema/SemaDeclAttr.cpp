323eda9be151ddf22c130202a4352617aedaede7
Jordan Rose <jordan_rose@apple.com>
2015-06-19 09:23:19 -0700

    Allow swift_name on classes, mainly for Swift's generated headers.
    There are problems putting swift_name on arbitrary classes and protocols
    because it interferes with lookup, but for interfaces generated by
    Swift itself it only needs to match up the declaration with the
    corresponding Swift declaration.
    
    Because of the aforementioned problems, make the "swift_name cannot go
    on this decl" error more vague, so that it doesn't promise features we
    haven't implemented.
    
    Part of rdar://problem/17469485

--- lib/Sema/SemaDeclAttr.cpp
+++ lib/Sema/SemaDeclAttr.cpp
@@ -4216,7 +4216,7 @@ static void handleSwiftName(Sema &S, Decl *D, const AttributeList &Attr) {
   if (auto *Method = dyn_cast<ObjCMethodDecl>(D)) {
     // swift_name only applies to factory methods for now.
     if (!Method->isClassMethod()) {
-      S.Diag(Attr.getLoc(), diag::err_attr_swift_name_decl_kind)
+      S.Diag(Attr.getLoc(), diag::err_attr_swift_name_factory_methods_only)
           << Attr.getName();
       return;
     }
@@ -4229,7 +4229,7 @@ static void handleSwiftName(Sema &S, Decl *D, const AttributeList &Attr) {
       HasRelatedResultType = (RTC == Sema::RTC_Compatible);
     }
     if (!HasRelatedResultType) {
-      S.Diag(Attr.getLoc(), diag::err_attr_swift_name_decl_kind)
+      S.Diag(Attr.getLoc(), diag::err_attr_swift_name_factory_methods_only)
           << Attr.getName();
       return;
     }
@@ -4250,7 +4250,8 @@ static void handleSwiftName(Sema &S, Decl *D, const AttributeList &Attr) {
       return;
     }
 
-  } else if (isa<EnumConstantDecl>(D) || isa<ObjCProtocolDecl>(D)) {
+  } else if (isa<EnumConstantDecl>(D) || isa<ObjCProtocolDecl>(D) ||
+             isa<ObjCInterfaceDecl>(D)) {
     if (!isValidIdentifier(Name)) {
       S.Diag(ArgLoc, diag::err_attr_swift_name_identifier) << Attr.getName();
       return;
