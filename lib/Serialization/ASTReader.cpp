3cb72e0fd8ae46b31f586c50bf27bbe04ebed53f
Jordan Rose <jordan_rose@apple.com>
2015-06-23 10:54:19 -0700

    Revert "Add a hack workaround for <rdar://problem/21480635> Swift can't import macros anymore"
    We can do this on the Swift side and it should work fine.

--- lib/Serialization/ASTReader.cpp
+++ lib/Serialization/ASTReader.cpp
@@ -3279,7 +3279,6 @@ void ASTReader::makeModuleVisible(Module *Mod,
 
     // Update the module's name visibility.
     Mod->NameVisibility = NameVisibility;
-    PP.makeModuleVisible(Mod, ImportLoc);
 
     // If we've already deserialized any names from this module,
     // mark them as visible.
