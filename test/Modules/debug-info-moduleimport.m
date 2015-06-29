7b4707c05d0e9b3edd11ce240308c2261c08d8d0
Adrian Prantl <aprantl@apple.com>
2015-06-29 12:02:01 -0700

    Update clang to match llvm changes.

--- test/Modules/debug-info-moduleimport.m
+++ test/Modules/debug-info-moduleimport.m
@@ -1,8 +1,7 @@
 // RUN: rm -rf %t
-// RUN: %clang_cc1 -g -dwarf-ext-refs -fmodules -DMODULES -fimplicit-module-maps -fmodules-cache-path=%t %s -I %S/Inputs -I %t -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 -g -dwarf-ext-refs -fmodules -DMODULES -fimplicit-module-maps -fmodules-cache-path=%t %s -I %S/Inputs -isysroot /tmp/.. -I %t -emit-llvm -o - | FileCheck %s
   
 // CHECK: ![[CU:.*]] = distinct !DICompileUnit
 @import DebugModule;
 // CHECK: !DIImportedEntity(tag: DW_TAG_imported_declaration, scope: ![[CU]], entity: ![[MODULE:.*]], line: 5)
-// CHECK: ![[MODULE]] = !MDModule(name: "DebugModule", scope: null, file: ![[PCM:.*]])
-// CHECK: ![[PCM]] = {{.*}}.pcm
+// CHECK: ![[MODULE]] = !DIModule(scope: null, name: "DebugModule", configMacros: "-DMODULES=0", includePath: "{{.*}}/test/Modules/Inputs", isysroot: "/tmp/..")
