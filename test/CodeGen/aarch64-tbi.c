2dd6d45a2f20b3940ca3f94be40bfc022824b71a
Steven Wu <stevenwu@apple.com>
2015-07-08 11:39:23 -0700

    Use cc1 in aarch64-tbi test
    A driver improvement is causing this test to be failed with Gala SDK.
    Using cc1 directly since this is a CodeGen test.

--- test/CodeGen/aarch64-tbi.c
+++ test/CodeGen/aarch64-tbi.c
@@ -1,10 +1,10 @@
 // REQUIRES: aarch64-registered-target
 
-// RUN: %clang -target arm64-apple-ios8.0 -mcpu=cyclone \
+// RUN: %clang_cc1 -triple arm64-apple-ios8.0 -target-cpu cyclone \
 // RUN:   -S -O3 -o - %s | \
 // RUN:   FileCheck --check-prefix=BOTH --check-prefix=NO_TBI %s
 
-// RUN: %clang -target arm64-apple-ios8.0 -mcpu=cyclone \
+// RUN: %clang_cc1 -triple arm64-apple-ios8.0 -target-cpu cyclone \
 // RUN:   -S -O3 -o - %s -mllvm -aarch64-use-tbi | \
 // RUN:   FileCheck --check-prefix=BOTH --check-prefix=TBI %s
 
