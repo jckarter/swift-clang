8f77095a4c8f530d771197e573a6941139908db8
Bob Wilson <bob.wilson@apple.com>
2015-08-10 15:58:39 -0700

    Update hasSubscripting check to require OS X 10.11 or iOS 9.
    In OS X 10.11 and iOS 9, NSMutableDictionary now allows you to use its
    subscript syntax to assign nil for a key. For earlier versions, we need to
    link arclite to get support for that. rdar://problem/22207160

--- test/Driver/arclite-link.c
+++ test/Driver/arclite-link.c
@@ -1,6 +1,6 @@
 // RUN: touch %t.o
-// RUN: %clang -### -target x86_64-apple-darwin10 -fobjc-link-runtime -lfoo -mmacosx-version-min=10.7 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-OSX %s
-// RUN: %clang -### -target x86_64-apple-darwin10 -fobjc-link-runtime -mmacosx-version-min=10.8 %t.o 2>&1 | FileCheck -check-prefix=CHECK-NOARCLITE %s
+// RUN: %clang -### -target x86_64-apple-darwin10 -fobjc-link-runtime -lfoo -mmacosx-version-min=10.10 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-OSX %s
+// RUN: %clang -### -target x86_64-apple-darwin10 -fobjc-link-runtime -mmacosx-version-min=10.11 %t.o 2>&1 | FileCheck -check-prefix=CHECK-NOARCLITE %s
 // RUN: %clang -### -target i386-apple-darwin10 -fobjc-link-runtime -mmacosx-version-min=10.7 %t.o 2>&1 | FileCheck -check-prefix=CHECK-NOARCLITE %s
 // RUN: %clang -### -target x86_64-apple-darwin10 -fobjc-link-runtime -nostdlib %t.o 2>&1 | FileCheck -check-prefix=CHECK-NOSTDLIB %s
 
@@ -12,18 +12,18 @@
 // CHECK-NOARCLITE-NOT: libarclite
 // CHECK-NOSTDLIB-NOT: -lobjc
 
-// RUN: %clang -### -target x86_64-apple-darwin10 -fobjc-link-runtime -fobjc-arc -mmacosx-version-min=10.7 %s 2>&1 | FileCheck -check-prefix=CHECK-UNUSED %s
+// RUN: %clang -### -target x86_64-apple-darwin10 -fobjc-link-runtime -fobjc-arc -mmacosx-version-min=10.10 %s 2>&1 | FileCheck -check-prefix=CHECK-UNUSED %s
 
 // CHECK-UNUSED-NOT: warning: argument unused during compilation: '-fobjc-link-runtime'
 
-// RUN: %clang -### -target arm64-apple-ios -fobjc-link-runtime -lfoo -mios-version-min=4.0 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-IOS %s
+// RUN: %clang -### -target arm64-apple-ios -fobjc-link-runtime -lfoo -mios-version-min=8.0 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-IOS %s
 // CHECK-ARCLITE-IOS: libarclite_iphoneos.a
 
-// RUN: %clang -### -target x86_64-apple-ios -fobjc-link-runtime -mios-version-min=4.0 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-IOSSIM %s
+// RUN: %clang -### -target x86_64-apple-ios -fobjc-link-runtime -mios-version-min=8.0 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-IOSSIM %s
 // CHECK-ARCLITE-IOSSIM: libarclite_iphonesimulator.a
 
-// RUN: %clang -### -target armv7-apple-tvos -fobjc-link-runtime -mtvos-version-min=4.0 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-TVOS %s
+// RUN: %clang -### -target armv7-apple-tvos -fobjc-link-runtime -mtvos-version-min=8.0 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-TVOS %s
 // CHECK-ARCLITE-TVOS: libarclite_appletvos.a
 
-// RUN: %clang -### -target i386-apple-tvos -fobjc-link-runtime -mtvos-version-min=4.0 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-TVSIM %s
+// RUN: %clang -### -target i386-apple-tvos -fobjc-link-runtime -mtvos-version-min=8.0 %t.o 2>&1 | FileCheck -check-prefix=CHECK-ARCLITE-TVSIM %s
 // CHECK-ARCLITE-TVSIM: libarclite_appletvsimulator.a
