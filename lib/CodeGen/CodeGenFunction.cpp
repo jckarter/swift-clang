40b4361d25bbce20d7cfd795bd045f1d2ca7bc62
Bob Wilson <bob.wilson@apple.com>
2015-08-26 13:45:12 -0700

    Re-re-revert most of r165273.
    This change was reverted from our master branch due to rdar://problem/15055872.
    The revert was accidentally lost when resolving a merge conflict: see
    8e7ee5ae7d1540a73fa6c7f03195061dee6e1f1d.
    
    See rdar://problem/15059304.

--- lib/CodeGen/CodeGenFunction.cpp
+++ lib/CodeGen/CodeGenFunction.cpp
@@ -926,11 +926,7 @@ void CodeGenFunction::GenerateCode(GlobalDecl GD, llvm::Function *Fn,
       EmitCheck(std::make_pair(IsFalse, SanitizerKind::Return),
                 "missing_return", EmitCheckSourceLocation(FD->getLocation()),
                 None);
-    } else if (CGM.getCodeGenOpts().OptimizationLevel == 0) {
-      EmitTrapCall(llvm::Intrinsic::trap);
     }
-    Builder.CreateUnreachable();
-    Builder.ClearInsertionPoint();
   }
 
   // Emit the standard function epilogue.
