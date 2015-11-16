3b44aac6e1d623d2b0fbb3903d6c441c52ee9763
John McCall <rjmccall@apple.com>
2015-11-16 15:25:58 -0800

    Don't honor -Werror on the -fignore-objc-weak warning.

--- test/SemaObjC/mrc-weak-ignored.m
+++ test/SemaObjC/mrc-weak-ignored.m
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fobjc-runtime-has-weak -fignore-objc-weak -fsyntax-only -verify %s
+// RUN: %clang_cc1 -fobjc-runtime-has-weak -fignore-objc-weak -Werror -fsyntax-only -verify %s
 
 __attribute__((objc_root_class))
 @interface Root @end
