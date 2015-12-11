82b23862a407c9dcc3c8c812f2fa54301383549c
Chris Bieneman <beanz@apple.com>
2015-12-11 13:50:53 -0800

    [CMake] <rdar://problem/23850929> CMake build is not honoring the top-level Clang_Version variable

--- utils/buildit/Project.mk
+++ utils/buildit/Project.mk
@@ -2,4 +2,4 @@ BUILD_CLANG = $(shell find $(SRCROOT) -name build_clang)
 
 clang: $(OBJROOT) $(SYMROOT) $(DSTROOT)
 	cd $(OBJROOT) && \
-		$(BUILD_CLANG) $(Clang_Use_Assertions) $(Clang_Use_Optimized)
+		$(BUILD_CLANG) $(Clang_Use_Assertions) $(Clang_Use_Optimized) $(Clang_Version)
